d834ac0805461e81f4e9bf689298236b6e864fb5
Rudichev <125951437+Kl1mvill@users.noreply.github.com>
2023-10-30 15:16:40 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# pip install pygame
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode()
+image = pygame.image.load("background.png").convert()
+music = pygame.mixer.Sound("music.ogg")
+
+music.play()  # Воспроизведение звука
+screen.blit(image, (0, 0))
+pygame.time.delay(100)  # Задержка на 0.1 секунды
+pygame.display.set_caption("0")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: ...
+    screen.blit(image, (0, 0))
+    pygame.display.flip()
